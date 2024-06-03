f592c1e5af5749c5d82dccd16d64c59626048538
uxsergey <sergeyxdesign@gmail.com>
2024-06-03 10:39:17 +0000

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>👨‍🍳 UX Cookbook</span>,
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  footer: {
+    text: "👨‍🍳 UX Cookbook ",
+  },
+};
+
+export default config;
